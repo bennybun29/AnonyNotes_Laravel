eaf238bd82f7fddc94be4aa7bc67d3132308c6a2
Aaron Tamayo <rebootedaarontamayo@gmail.com>
2024-10-19 22:10:43 +0800

    routes modify ulit

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ Route::get('users/{id}', [UsersController::class, 'show']);// Route for retrievi
 Route::delete('users/{id}', [UsersController::class, 'destroy']);// Route for deleting a specific user
 Route::put('users/{id}', [UsersController::class, 'update']);// Route for updating a specific user
 Route::get('/users/{id}/bio', [UsersController::class, 'getUserBio']);// Route for retrieving a specific user's bio
+Route::get('/users/{id}/notes', [UsersController::class, 'getUserNotes']);// Route for retrieving all notes for a specific user
 
 
 
@@ -26,21 +27,18 @@ Route::get('notes', [NotesController::class, 'index']);// Route for retrieving a
 Route::get('notes/{id}', [NotesController::class, 'show']);// Route for retrieving a specific note
 Route::delete('notes/{id}', [NotesController::class, 'destroy']);// Route for deleting a specific note
 Route::put('notes/{id}', [NotesController::class, 'update']);// Route for updating a specific note
-Route::get('/users/{id}/notes', [UsersController::class, 'getUserNotes']);// Route for retrieving all notes for a specific user
 
 //routes for comments CRUD
 Route::get('comments', [CommentsController::class, 'index']);// Route for retrieving all comments
 Route::get('comments/{id}', [CommentsController::class, 'show']);// Route for retrieving a specific comment
 Route::delete('comments/{id}', [CommentsController::class, 'destroy']);// Route for deleting a specific comment
 Route::put('comments/{id}', [CommentsController::class, 'update']);// Route for updating a specific comment
-Route::get('/users/{id}/comments', [UsersController::class, 'getUserComments']);// Route for retrieving all comments for a specific user
 
 //routes for hearts CRUD
 Route::get('hearts', [HeartsController::class, 'index']);// Route for retrieving all hearts
 Route::get('hearts/{id}', [HeartsController::class, 'show']);// Route for retrieving a specific heart
 Route::delete('hearts/{id}', [HeartsController::class, 'destroy']);// Route for deleting a specific heart
 Route::put('hearts/{id}', [HeartsController::class, 'update']);// Route for updating a specific heart
-Route::get('/users/{id}/hearts', [UsersController::class, 'getUserHearts']);// Route for retrieving all hearts for a specific user
 
 //Route for Change Password
 Route::post('change-password', [UsersController::class, 'changePassword'])->middleware('auth:sanctum');
